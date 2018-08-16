ff095ddbaaae60390ecbb656afe3f17861489409
Joana Chavez <chavezlucero88@gmail.com>
2018-08-16 14:14:58 +0200

    dark theme

---
+++ _build/html/_static/js/switch.js
@@ -0,0 +1,16 @@
+document.write('<div>\
+  <div class="night_icon">\
+  <i class="fa fa-moon"></i>\
+</div>\
+<div class="switcher">\
+  <label class="switch">\
+\
+      <input type="checkbox" checked>\
+      <span class="slider round"></span>\
+  </label>\
+</div>\
+<div class="sun_icon">\
+<i class="fa fa-sun"></i>\
+</div>\
+</div>\
+            ');
