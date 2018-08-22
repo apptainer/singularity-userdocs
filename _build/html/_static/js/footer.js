892d691b86cb084f732cc2a8bacfcd4c9dca819f
Joana Chavez <chavezlucero88@gmail.com>
2018-08-22 21:29:47 +0200

    version 2.6

--- _build/html/_static/js/footer.js
+++ _build/html/_static/js/footer.js
@@ -7,12 +7,12 @@ document.write('<div class="rst-versions" data-toggle="rst-versions" role="note"
    <div class="rst-other-versions">\
      <dl>\
        <dt>Versions</dt>\
-         <dd><a href="https://www.sylabs.io/guides/2.6.0/user-guide/">latest</a></dd>\
+         <dd><a href="https://www.sylabs.io/guides/2.6/user-guide/">v2.6</a></dd>\
      </dl>\
      <dl>\
        <dt>Downloads</dt>\
        <dd>\
-         <a href="https://www.sylabs.io/guides/2.6.0/user-guide.pdf">Latest PDF</a>\
+         <a href="https://www.sylabs.io/guides/2.6/user-guide.pdf">v2.6 PDF</a>\
        </dd>\
      </dl>\
      <dl>\
