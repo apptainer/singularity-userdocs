e264ccb103b8216b3a5f02ce226697219dab5d84
Joana Chavez <chavezlucero88@gmail.com>
2019-01-24 01:51:09 +0100

    sylabs inc

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Singularity container'
-copyright = u'2017-2019, Sylabs Inc.'
+copyright = u'2017-2019, Sylabs Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
