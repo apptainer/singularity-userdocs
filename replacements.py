a8ebc571a160e957433fe7ae443243722f76df7b
edytuk <38504203+edytuk@users.noreply.github.com>
2021-12-13 16:18:01 +0100

    Bump for 3.8.5 (#432)

--- replacements.py
+++ replacements.py
@@ -12,7 +12,7 @@ def variableReplace(app, docname, source):
 # Add the needed variables to be replaced either on code or on text on the next
 # dictionary structure.
 variable_replacements = {
-    "{InstallationVersion}" : "3.8.0",
+    "{InstallationVersion}" : "3.8.5",
     "\{admindocs\}" : "https://singularity.hpcng.org/admin-docs/3.8",
     "\{version\}": "3.8",
     "\{adminversion\}": "3.8",
